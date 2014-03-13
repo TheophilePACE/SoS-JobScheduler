dbe112017ab4ddbede0a006d831ed9b2e0e99ac4
m329997 <cetorcq@airfrance.fr>
2014-03-13 14:35:47 +0000

    Update .gitignore

--- dashboard/src/main/java/org/jobscheduler/dashboard/controller/FilterController.java
+++ dashboard/src/main/java/org/jobscheduler/dashboard/controller/FilterController.java
@@ -1,9 +1,12 @@
 package org.jobscheduler.dashboard.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.jobscheduler.dashboard.model.Field;
 import org.jobscheduler.dashboard.model.Filter;
+import org.jobscheduler.dashboard.repository.FieldRepository;
+import org.jobscheduler.dashboard.repository.FilterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,19 +20,38 @@ import com.wordnik.swagger.annotations.Api;
 @Api(value = "", description = "Filter")
 public class FilterController {
 
-	List<Filter> filters = new ArrayList<Filter>();
+	@Autowired
+	FilterRepository filterRepository;
 	
-	@RequestMapping(value = "/addFilter", method = RequestMethod.GET)
+	@Autowired
+	FieldRepository fieldRepository;
+
+	@RequestMapping(value = "/addFieldInFilter", method = RequestMethod.GET)
 	public @ResponseBody
 	Filter addFilter(
 			@RequestParam(value = "name", required = false) String name,
-			@RequestParam(value = "field", required = false) String field,
-			@RequestParam(value = "value", required = false) String value,
+			@RequestParam(value = "fieldName", required = false) String fieldName,
+			@RequestParam(value = "fieldValue", required = false) String fieldValue,
 			Model model) {
-		//filter.addField(field, value);
-		//return filter;
-		return null;
+		// Search existing filter
+		Filter filter = filterRepository.findByName(name);
+System.out.println("FilterController.addFilter()" + filter);
+		if (filter == null) {
+			filter = new Filter();
+			filter.setName(name);
+			filter = filterRepository.save(filter);
+		} 
 		
+		Field field = new Field();
+		field.setName(fieldName);
+		field.setValue(fieldValue);
+		field.setFilter(filter);
+		fieldRepository.save(field);
+		filter.addField(field);
+		filterRepository.save(filter);
+		
+		return filter;
+
 	}
 
 }
