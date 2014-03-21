c22ee8556b48824d3f82a9c16931f3829ca2ed1c
m329997 <cetorcq@airfrance.fr>
2014-03-21 17:24:10 +0000

    Repair simple test

--- dashboard/src/test/java/org/jobscheduler/dashboard/ApplicationTests.java
+++ dashboard/src/test/java/org/jobscheduler/dashboard/ApplicationTests.java
@@ -2,12 +2,19 @@ package org.jobscheduler.dashboard;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.SpringApplicationContextLoader;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = Application.class, loader=SpringApplicationContextLoader.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@ActiveProfiles("dev")
 public class ApplicationTests {
 
 	@Test
